9518c6ab1518326452fb14e21fcde076426e38f2
Christina Sørensen <christina@cafkafk.com>
2024-02-24 14:31:59 +0100

    build(flake): add simple flake
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, systems, nixpkgs }: let
+    eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+  in {
+
+    packages = eachSystem (pkgs:
+      rec {
+        default = xscreensaver;
+        xscreensaver = pkgs.xscreensaver // { src = ./.; };
+      }
+    );
+
+    devShells = eachSystem (pkgs:
+      let
+        inherit pkgs;
+      in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [gnumake];
+          };
+        });
+  };
+}
